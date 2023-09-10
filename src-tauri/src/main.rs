ad302ef476336fc75c036724b57245197d53814e
Zzrk <1252124617@qq.com>
2023-09-11 00:57:10 +0800

    feat: start stop and staus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,15 +3,25 @@
 use enigo::*;
 
 #[tauri::command]
+fn mousescroll() {
+    // 鼠标滚轮事件
+    println!("mousescroll");
+    let mut enigo: Enigo = Enigo::new();
+    enigo.mouse_scroll_y(10);
+}
+
+#[tauri::command]
 fn mousemove() {
+    // 鼠标移动和点击事件
     println!("mousemove");
     let mut enigo = Enigo::new();
-    enigo.mouse_move_relative(100, 100);
+    enigo.mouse_move_to(1250, 1250);
+    enigo.mouse_click(MouseButton::Left);
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![mousemove])
+        .invoke_handler(tauri::generate_handler![mousescroll, mousemove])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,50 @@
 <script setup lang="ts">
-import { invoke } from '@tauri-apps/api/tauri'
-import { ref } from 'vue';
+import { invoke } from '@tauri-apps/api/tauri';
+import { register, unregister } from '@tauri-apps/api/globalShortcut';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const wereadRef = ref<HTMLIFrameElement>();
+// 当前是否处于运行状态
+const isWorking = ref(false);
 
-const handleClick = () => {
-  invoke("mousemove")
+// 任务函数
+const work = () => {
+  invoke("mousemove");
+  invoke("mousescroll");
+  setTimeout(() => {
+    if (!isWorking.value) return;
+    work();
+  }, 2000);
 }
+
+// 启动任务
+const handleStart = () => {
+  isWorking.value = true;
+  work();
+}
+
+// 停止任务
+const handleStop = () => {
+  isWorking.value = false;
+}
+
+// 停止任务快捷键的注册和删除
+onMounted(() => {
+  register('CommandOrControl+Shift+C', handleStop)
+})
+onUnmounted(() => {
+  unregister('CommandOrControl+Shift+C')
+})
 </script>
 
 <template>
   <div class="container">
     <div class="menu-bar">
-      <button @click="handleClick">一键启动</button>
+      <div>
+        <button @click="handleStart">启动</button>
+        <button @click="handleStop">停止</button>
+      </div>
+      <span>{{ isWorking ? '运行中' : '已停止' }}</span>
     </div>
     <div class="content">
       <iframe ref="wereadRef" src="https://weread.qq.com/" frameborder="0"></iframe>
@@ -26,6 +58,12 @@ const handleClick = () => {
   height: 100%;
   display: flex;
   flex-direction: column;
+}
+
+.menu-bar {
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
 }
 
 .content {
